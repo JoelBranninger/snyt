01b1fbfcb76979f8ed7b29922c1a1495629cd1aa
Joel Bränninger <joel.branninger@live.se>
2024-06-04 21:03:39 +0200

    remove api version

--- sanity/sanity.client.ts
+++ sanity/sanity.client.ts
@@ -5,7 +5,7 @@ import { createClient, type ClientConfig } from '@sanity/client'
 const config: ClientConfig = {
   projectId: 'gqdlozy4',
   dataset: 'production',
-  apiVersion: '2024-06-05',
+  // apiVersion: '2024-06-05',
   useCdn: false,
 }
 
